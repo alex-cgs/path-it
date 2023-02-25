0560d4fc5a3159d258a2cc83ec7da3fa421c46df
CookNChips <alexandretinouert@gmail.com>
2023-02-25 15:34:15 +0100

    Added Tanh and TanhDer in doc.js

---
+++ doc.js
@@ -0,0 +1,14 @@
+function MatMult(Mat1, Mat2) {
+    result = new Array();
+    for (let i = 0; i < Mat1.length; i++) {
+        
+    }
+}
+
+function TanH(x) {
+    return Math.tanh(x);
+}
+
+function TanHder(x) {
+    return 1 - TanH(x)**2;
+}
