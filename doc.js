1da7460b72b4c76db131c30ff1093cb47517b1fd
CookNChips <alexandretinouert@gmail.com>
2023-02-25 17:03:33 +0100

    Can perform matrix multiplications

--- doc.js
+++ doc.js
@@ -1,15 +1,20 @@
-export function MatMult(Mat1, Mat2) {
-    result = new Array();
+export function matMult(Mat1, Mat2) {
+    result = new Array(Mat1.length);
     //Mat1's row length must be equal to Mat2's column length
     if (Mat1.length != Mat2[0].length) {
         return 0;
     }
+    for (let i = 0; i < Mat1.length; i++) {
+        for (let j = 0; j < Mat2[i].length; j++) {
+
+        }
+    }
 }
 
-export function TanH(x) {
+export function tanh(x) {
     return Math.tanh(x);
 }
 
-export function TanHder(x) {
+export function tanhDer(x) {
     return 1 - TanH(x)**2;
 }
--- script.js
+++ script.js
@@ -2,11 +2,39 @@
 
 map = Array(10).fill(Array(10).fill(1));
 
-function ScalMult(Mat, Scalar) {
+function scalMult(Mat, Scalar) {
     for (var i = 0; i < Mat.length; i++) {
         Mat[i] = Mat[i].map(function(x) { return x * Scalar; });
     }
     return Mat;
 }
 
-console.log(ScalMult(map, 10));
+function matrixMultiplication(Mat1, Mat2) {
+    // Check if the matrices can be multiplied
+    if (Mat1[0].length !== Mat2.length) {
+      throw "Uncompatible matrices!";
+    }
+  
+    // Create the result matrix
+    const result = new Array(Mat1.length);
+    for (let i = 0; i < result.length; i++) {
+      result[i] = new Array(Mat2[0].length);
+    }
+  
+    // Perform the matrix multiplication
+    for (let i = 0; i < result.length; i++) {
+      for (let j = 0; j < result[i].length; j++) {
+        let newval = 0;
+        for (let k = 0; k < Mat1[i].length; k++) {
+          newval += Mat1[i][k] * Mat2[k][j];
+        }
+        result[i][j] = newval;
+      }
+    }
+  
+    // Return the result matrix
+    return result;
+}
+
+
+console.log(MatMult([[3, 2, 1], [13, 3, 19], [3, 9, 10]], [[2, 8, 9], [1, 9, 0], [8, 8, 2]]));
