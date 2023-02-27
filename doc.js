0307a629fd5b415f78a99af5737816049cc23ae7
CookNChips <alexandretinouert@gmail.com>
2023-02-27 16:05:14 +0100

    Fixed JS import export modules

--- doc.js
+++ doc.js
@@ -1,20 +1,71 @@
-export function matMult(Mat1, Mat2) {
-    result = new Array(Mat1.length);
-    //Mat1's row length must be equal to Mat2's column length
-    if (Mat1.length != Mat2[0].length) {
-        return 0;
+export function scalMult(Mat, Scalar) {
+    for (var i = 0; i < Mat.length; i++) {
+        Mat[i] = Mat[i].map(function(x) { return x * Scalar; });
     }
-    for (let i = 0; i < Mat1.length; i++) {
-        for (let j = 0; j < Mat2[i].length; j++) {
-
-        }
-    }
+    return Mat;
 }
+
+export function softmax(Mat) {
+    const result = new Array();
+    for (let i = 0; i < Mat.length; i++) {
+      const row = Mat[i];
+      const rowExp = row.map((value) => Math.exp(value));
+      const rowExpSum = rowExp.reduce((accumulator, value) => accumulator + value, 0);
+      const rowSoftmax = rowExp.map((value) => value / rowExpSum);
+      result.push(rowSoftmax);
+    }
+    return result;
+}
+
+export function softmaxDerivative(Mat) {
+    const result = new Array();
+    for (let i = 0; i < Mat.length; i++) {
+      const row = Mat[i];
+      const rowExp = row.map((value) => Math.exp(value));
+      const rowExpSum = rowExp.reduce((accumulator, value) => accumulator + value, 0);
+      const rowSoftmax = rowExp.map((value) => value / rowExpSum);
+      const rowDerivative = [];
+      for (let j = 0; j < row.length; j++) {
+        const kroneckerDelta = (i === j) ? 1 : 0;
+        rowDerivative.push(rowSoftmax[j] * (kroneckerDelta - rowSoftmax[i]));
+      }
+      result.push(rowDerivative);
+    }
+    return result;
+}
+
 
 export function tanh(x) {
     return Math.tanh(x);
 }
 
 export function tanhDer(x) {
-    return 1 - TanH(x)**2;
+    return 1 - tanh(x)**2;
+}
+
+export function matMult(Mat1, Mat2) {
+    // Check if the matrices can be multiplied
+    if (Mat1[0].length !== Mat2.length) {
+      throw "Uncompatible matrices!";
+    }
+  
+    // Create the result matrix
+    const result = new Array(Mat1.length);
+    for (let i = 0; i < result.length; i++) {
+      result[i] = new Array(Mat2[0].length);
+    }
+  
+    // Perform the matrix multiplication
+    for (let i = 0; i < result.length; i++) {
+      for (let j = 0; j < result[i].length; j++) {
+        let newval = 0;
+        for (let k = 0; k < Mat1[i].length; k++) {
+          newval += Mat1[i][k] * Mat2[k][j];
+        }
+        result[i][j] = newval;
+      }
+    }
+  
+    // Return the result matrix
+    return result;
 }
--- script.js
+++ script.js
@@ -1,40 +1,5 @@
-//import * as doc from 'doc';
+import * as doc from './doc.js';
 
-map = Array(10).fill(Array(10).fill(1));
+let map = Array(10).fill(Array(10).fill(1));
 
-function scalMult(Mat, Scalar) {
-    for (var i = 0; i < Mat.length; i++) {
-        Mat[i] = Mat[i].map(function(x) { return x * Scalar; });
-    }
-    return Mat;
-}
-
-function matrixMultiplication(Mat1, Mat2) {
-    // Check if the matrices can be multiplied
-    if (Mat1[0].length !== Mat2.length) {
-      throw "Uncompatible matrices!";
-    }
-  
-    // Create the result matrix
-    const result = new Array(Mat1.length);
-    for (let i = 0; i < result.length; i++) {
-      result[i] = new Array(Mat2[0].length);
-    }
-  
-    // Perform the matrix multiplication
-    for (let i = 0; i < result.length; i++) {
-      for (let j = 0; j < result[i].length; j++) {
-        let newval = 0;
-        for (let k = 0; k < Mat1[i].length; k++) {
-          newval += Mat1[i][k] * Mat2[k][j];
-        }
-        result[i][j] = newval;
-      }
-    }
-  
-    // Return the result matrix
-    return result;
-}
-
-
-console.log(MatMult([[3, 2, 1], [13, 3, 19], [3, 9, 10]], [[2, 8, 9], [1, 9, 0], [8, 8, 2]]));
+console.log(doc.softmax([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
